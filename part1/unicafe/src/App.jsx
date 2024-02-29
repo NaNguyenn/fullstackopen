fb927b630a7b6224455953f4fbe194047f1bf1ff
NaNguyen <yu19151998@gmail.com>
2024-02-29 16:59:56 +0700

    done 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,11 +4,40 @@ const Button = ({ label, onClick }) => {
   return <button onClick={onClick}>{label}</button>;
 };
 
-const Statistic = ({ label, value }) => {
+const Statistic = ({ label, value, unit }) => {
   return (
     <div>
       <span>{label} </span>
       <span>{value}</span>
+      <span> {unit}</span>
+    </div>
+  );
+};
+
+const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  averageScore,
+  positiveRate,
+}) => {
+  const statisticsShown = good !== 0 || neutral !== 0 || bad !== 0;
+  return (
+    <div>
+      <h1>statistics</h1>
+      {statisticsShown ? (
+        <div>
+          <Statistic label="good" value={good} />
+          <Statistic label="neutral" value={neutral} />
+          <Statistic label="bad" value={bad} />
+          <Statistic label="all" value={total} />
+          <Statistic label="average" value={averageScore} />
+          <Statistic label="positive" value={positiveRate} unit="%" />
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
@@ -18,6 +47,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const averageScore = (good - bad) / total;
+  const positiveRate = (good * 100) / total;
 
   const handleGoodButtonClick = () => setGood(good + 1);
   const handleNeutralButtonClick = () => setNeutral(neutral + 1);
@@ -29,10 +61,14 @@ const App = () => {
       <Button label={"good"} onClick={handleGoodButtonClick} />
       <Button label={"neutral"} onClick={handleNeutralButtonClick} />
       <Button label={"bad"} onClick={handleBadButtonClick} />
-      <h1>statistics</h1>
-      <Statistic label="good" value={good} />
-      <Statistic label="neutral" value={neutral} />
-      <Statistic label="bad" value={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        averageScore={averageScore}
+        positiveRate={positiveRate}
+      />
     </div>
   );
 };
