b77e43851cd28b7e8f44b0a133c5f44b4315d2b8
NaNguyen <yu19151998@gmail.com>
2024-09-18 16:48:28 +0700

    done 3.22

--- part3/phonebook-be/index.js
+++ part3/phonebook-be/index.js
@@ -1,119 +1,129 @@
-const express = require("express");
-var morgan = require("morgan");
-const cors = require("cors");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+var morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+const app = express()
 
-app.use(cors());
-app.use(express.static("dist"));
-app.use(express.json());
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
 
-morgan.token("reqBody", function (req, res) {
-  return JSON.stringify(req.body);
-});
+morgan.token('reqBody', function (req) {
+  return JSON.stringify(req.body)
+})
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :reqBody"
+    ':method :url :status :res[content-length] - :response-time ms :reqBody'
   )
-);
+)
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const dateNow = new Date()
 
-const dateNow = new Date();
+app.get('/info', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.send(
+      `<p>Phonebook has info for ${
+        persons.length
+      } people</p><p>${dateNow.toString()}</p>`
+    )
+  })
+})
 
-const generateId = () => {
-  return String(Math.floor(Math.random() * 10000));
-};
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
+})
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${
-      persons.length
-    } people</p><p>${dateNow.toString()}</p>`
-  );
-});
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+  Person.findById(id)
+    .then((person) => {
+      if (!person) {
+        response.status(404).end()
+        return
+      }
+      response.json(person)
+    })
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
 
-  if (!person) {
-    response.status(404).end();
-    return;
-  }
-  response.json(person);
-});
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
+  // Person.find({}).then((persons) => {
+  //   if (persons.some((person) => person.name === body.name)) {
+  //     return response.status(400).json({
+  //       error: "name must be unique",
+  //     });
+  //   }
+  // });
 
-  response.status(204).end();
-});
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
+})
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: "name is required",
-    });
-  }
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: "number is required",
-    });
-  }
-
-  if (persons.some((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  }
 
-  persons = persons.concat(person);
-
-  response.json(person);
-});
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
