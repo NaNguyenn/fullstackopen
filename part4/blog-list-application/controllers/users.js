e5ea1976b8b3e1fe385dc9311adc20f2bd5b60a8
NaNguyen <yu19151998@gmail.com>
2025-02-05 14:38:31 +0700

    feat(part4): add min length validation for new user password

--- part4/blog-list-application/controllers/users.js
+++ part4/blog-list-application/controllers/users.js
@@ -5,6 +5,12 @@ const User = require("../models/user");
 usersRouter.post("/", async (request, response, next) => {
   const { username, name, password } = request.body;
 
+  if (password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "Password must be at least 3 characters" });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
