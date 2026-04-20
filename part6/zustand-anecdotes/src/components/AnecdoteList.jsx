60a0e6eb6ee4a42d2d190ddf2e226668c6a4526e
NaNguyenn <yu19151998@gmail.com>
2026-04-20 09:40:07 +0700

    feat(6.5): add sort anecdotes by number of votes

--- part6/zustand-anecdotes/src/components/AnecdoteList.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -1,12 +1,17 @@
+import { useMemo } from "react";
 import { useAnecdoteActions, useAnecdotes } from "../store";
 
 const AnecdoteList = () => {
   const anecdotes = useAnecdotes();
   const { vote } = useAnecdoteActions();
+  const sortedAnecdotes = useMemo(
+    () => anecdotes.toSorted((first, second) => second.votes - first.votes),
+    [anecdotes],
+  );
 
   return (
     <>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
